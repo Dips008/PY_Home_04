678f328d5a2df34bfbdf309b33e197e43b210334
Igor_Demchenko <idemch777@gmail.com>
2023-01-08 01:34:15 +0300

    Add files 01-03

--- Home_task_02.py
+++ Home_task_02.py
@@ -1,2 +1,19 @@
 # Задайте натуральное число N. Напишите программу,
 # которая составит список простых множителей числа N.
+
+n = int(input('Enter the number N = '))
+
+
+def multipliers(e):
+    i = 2
+    mult = []
+    while e > 1:
+        if e % i == 0:
+            mult.append(i)
+            e = e / i
+        else:
+            i += 1
+    return mult
+
+
+print(multipliers(n))
